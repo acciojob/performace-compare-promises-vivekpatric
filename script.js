f60ce84287dc31859de41c9a44092f9fcff7a78e
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-06-02 12:47:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,38 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+function fetchData(url) {
+  return fetch(url).then(response => response.json());
+}
 
+function fetchWithPromiseAll() {
+  const startTime = performance.now();
+  
+  Promise.all(apiUrls.map(fetchData))
+    .then(() => {
+      const endTime = performance.now();
+      const timeTaken = endTime - startTime;
+      document.getElementById("output-all").textContent = timeTaken.toFixed(2);
+    })
+    .catch(error => {
+      console.error("Promise.all failed:", error);
+    });
+}
+
+function fetchWithPromiseAny() {
+  const startTime = performance.now();
+
+  Promise.any(apiUrls.map(fetchData))
+    .then(() => {
+      const endTime = performance.now();
+      const timeTaken = endTime - startTime;
+      document.getElementById("output-any").textContent = timeTaken.toFixed(2);
+    })
+    .catch(error => {
+      console.error("Promise.any failed:", error);
+    });
+}
+
+fetchWithPromiseAll();
+fetchWithPromiseAny();
 // You can write your code here
